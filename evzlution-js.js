6b9b213302179887ad0fe7d13e9efe673493244b
Tazxor <79080127+Tazxor@users.noreply.github.com>
2023-03-22 12:10:43 +0100

    Initial commit

---
+++ evzlution-js.js
@@ -0,0 +1,18 @@
+let age = 25 ;// exo1 
+let nom = "jean"; // exo2
+let somme = 20+32; // exo3
+let chaine =  "contient une chaîne de caractères";// exo4
+console.log(chaine.toUpperCase());//exo4
+let tableau = [12,23,34,45,56,67,78,89,90]//exo5
+console.log(tableau.length)//exo5
+let objet = {
+    nom:'ozgul',
+    age:'30',
+}
+console.log(objet.nom)//exo6
+let nombre = 34.5343;//exo7
+console.log(nombre.toFixed(2));//exo7
+let date = new Date();//exo8
+console.log(date.getMonth()+ 1);//EXO8
+let tableau2 = ['paris', 'tokyo', 'strasbourg']//exo9
+console.log(tableau2.join(','))
